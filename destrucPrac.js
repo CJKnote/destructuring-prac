f96ed1d25f6d303299aa902c136d9ecb947e6388
Caroline Knote <cj.knote@gmail.com>
2021-12-22 23:17:26 -0800

    implementing functions and answering questions

---
+++ destrucPrac.js
@@ -0,0 +1,41 @@
+//the first part of this exercises is written in destrucExerciseWrittenQs.txt
+
+/*ES5 */
+// var obj = {
+//     numbers: {
+//       a: 1,
+//       b: 2
+//     }
+//   };
+  
+//   var a = obj.numbers.a;
+//   var b = obj.numbers.b;
+
+/* Write an ES2015 Version */
+const obj ={
+    numbers: {
+        a: 1,
+        b: 2
+    }
+};
+
+const {a, b} = obj.numbers;
+
+// var arr = [1, 2];
+// var temp = arr[0];
+// arr[0] = arr[1];
+// arr[1] = temp;
+
+/* Write an ES2015 Version */
+let arr = [1 ,2];
+[arr[0], arr[1]] = [arr[1], arr[0]];
+
+
+// Write a function called raceResults which accepts a single array argument. It should return an object with the keys first, second, third, and rest.
+
+// first: the first element in the array
+// second: the second element in the array
+// third: the third element in the array
+// rest: all other elements in the array
+
+const raceResults = ([first, second, third, ...rest]) => ({first, second, third, rest});
